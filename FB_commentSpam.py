de68fbe1be83a5fe547f6c3142ba2aac79fe0f0d
Pablo Castelo <pablo@castelo.mx>
2016-02-29 16:53:45 -0700

    share post

--- FB_commentSpam.py
+++ FB_commentSpam.py
@@ -1,9 +1,11 @@
+# https://github.com/blaklites/fb
+
 import json
 import fb                     #To install this package run: sudo pip install fb
 from facepy import GraphAPI   #To install this package run: sudo pip install facepy
 
 def spam():
-    token = "CAACEdEose0cBAKZCj1Cu3EFMlFJ6cnRiowpnNBAy5R0M0s58UDZCy2XLaZAPmi8KbuwY7fXSlc9yDza2aeuGV1fIHHwoYOndn8mtZAnkyoLVss4ahYsc5SJ02ZCeOQgrNcuinWhLRScaxfUynDWfWklIG2BCx0SBPTn0TNoc8SkfdtoPF8Y7KcJagwIZAe6Kyp38VwZAbFaxfW8vpK89h3lZAUSok0AZAFM8ZD"#Insert access token here.
+    token = "CAACEdEose0cBAG0mSWdU5cciKPGAbbXddyxdOkCna3WZCVcZCdHpftni158nt3sCJrkKVGGsaZAaYRKBZAkwfZAfHZCgAduToV1XDUEEFIJP5KwR61Yect2kOVKYFWlZC7SuPlfTdRJuwsHcYHZCgtMn8skjT02vbXID0Uld3otNu6VyrIWe76xmbf4bEx2w6XmLFRiOAT37X2q6v6Qgrer1hrZB3XW5YpNgZD"#Insert access token here.
     facebook = fb.graph.api(token)
     graph1 = GraphAPI(token)
 
@@ -25,10 +27,13 @@ def spam():
         mess = str(mess)
         if nos <= len(idlist):
            for indid in (idlist[(len(idlist)-nos):]):
-              facebook.publish(cat = "comments", id = indid, message = mess) #Comments on each post
-              facebook.publish(cat = "likes", id = indid)                 #Likes each post
+              #facebook.publish(cat = "comments", id = indid, message = mess) #Comments on each post
+              #facebook.publish(cat = "likes", id = indid)                 #Likes each post
+              #facebook.publish(cat = "share", id = indid)                 #Shares each post
+              post_to_post = "http://www.facebook.com/" + str(indid).split('_')[0] + "/posts/" + str(indid).split('_')[1]
+              facebook.publish(cat = "feed", id = "me", message="how funny is this", link = post_to_post)
               count = count + 1
-              print("Notification number: "+ str(count) + " on www.facebook.com/" + str(indid).split('_')[0] + "/posts/" + str(indid).split('_')[1])
+              print("Notification number: "+ str(count) + " on " + post_to_post)
         else:
               print("Not that many spammable posts available. No spam happening.")
     else:
